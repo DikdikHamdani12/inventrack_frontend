3178defc2e8463dba859673886bacb7bef184889
DikdikHamdani12 <dikdik@example.com>
2026-06-09 21:50:40 +0700

    Remove unused php hello world files

--- php_hello_world/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Simple PHP Hello World page
-
-echo '<!DOCTYPE html>';
-echo '<html lang="en">';
-echo '<head><meta charset="UTF-8"><title>Hello PHP</title></head>';
-echo '<body><h1>Hello, World!</h1><p>Selamat datang di proyek PHP sederhana.</p></body>';
-echo '</html>';
